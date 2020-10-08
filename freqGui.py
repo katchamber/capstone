812a8f002ebb22c433c3688bf0bc4f43f5723be6
Jack Mix <jackmixiii@gmail.com>
2020-10-07 19:44:37 -0700

    Updated on/off

--- freqGui.py
+++ freqGui.py
@@ -1,40 +1,67 @@
 from guizero import App, Text, TextBox, PushButton
 
 
-def getFreqValue():
-    if ((int(lightFreqInput.value) < 0) or (int(lightFreqInput.value) > 60)):
-        mainDisplay.value ="Light Frequency out of bounds"
-        lightFreq=0;
-    else:
-            lightFreq = int(lightFreqInput.value)
-    if ((int(audioFreqInput.value) < 0) or (int(audioFreqInput.value) > 60)):
-        mainDisplay.value ="Audio Frequency out of bounds"
-        audioFreq=0;
-    else:
-            audioFreq = int(audioFreqInput.value)
-    
-    
-            
-
-    
-freqGui = App(title="Brain Bot")
-
+#data I gotta send to Kathyrn
 lightFreq = 0
 audioFreq = 0
+runStatusAudio = False
+runStatusLight = False
+
+def getFreqValue():
+    mainDisplay.value = str(lightFreq)
+    if ((int(lightFreqInput.value) < 0) or (int(lightFreqInput.value) > 60)):
+        lightFreq=0
+        err1 = True
+    else:
+            lightFreq = int(lightFreqInput.value)
+            err1 = False
+    if ((int(audioFreqInput.value) < 0) or (int(audioFreqInput.value) > 60)):
+        audioFreq=0
+        err2 = True
+    else:
+            audioFreq = int(audioFreqInput.value)
+            err2 = False
+    if(err2 or err1):
+        errFlagBox.value = '*Error*'
+    else:
+            errFlagBox.value = 'Frequencies Updated'
+            
+    displayLight.value = "Light Frequency: " + str(lightFreq)
+    displayAudio.value = "Audio Frequency: " + str(audioFreq)
+    
+def onOff():
+    mainDisplay.value = yellow
+    runStatusLight = not runStatusLight
+    runStatusAudio = not runStatusAudio
+    onOffLight.text = str(runStatusLight)
+    onOffAudio.text = str(runStatusAudio)
+
+    
+freqGui = App(title="Brain Bot") #start of app
+
+
+
 
 mainDisplay = Text(freqGui, text="Welcome to the Brain Bot",size=36, font="Roboto") #main display
+
+
+
+
 lightTitle = Text(freqGui,text="Light Frequency",size=24,font="Roboto")
 lightFreqInput = TextBox(freqGui, width="40")
 audioTitle = Text(freqGui,text="Audio Frequency",size=24,font="Roboto")
 audioFreqInput = TextBox(freqGui, width="40")
 updateFreqValue = PushButton(freqGui,command=getFreqValue,text="Update Frequency Values")
 
-displayLight = Text(freqGui,text="Light Frequency: " + str(lightFreq),size=24,font="Roboto")
-displayAudio = Text(freqGui,text="Audio Frequency: " + str(audioFreq),size=24,font="Roboto")
+displayLight = Text(freqGui,text="No values set",size=24,font="Roboto")
+displayAudio = Text(freqGui,text="No values Set",size=24,font="Roboto")
+
+errFlagBox = Text(freqGui,text="**",size=24,font="Roboto")
+
+onOffLight = PushButton(freqGui,command=onOff,text="Light status: Standby")
+onOffAudio = PushButton(freqGui,command=onOff,text="Audio status: Standby")
 
 
+            
 
 freqGui.display()
-
-
-
