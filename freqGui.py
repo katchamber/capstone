dc05653ba0dcc7b9019be94fbf6a2335e1365fd2
Jack Mix <jackmixiii@gmail.com>
2020-10-16 18:33:26 -0700

    With comments now

--- freqGui.py
+++ freqGui.py
@@ -1,14 +1,18 @@
 from guizero import App, Text, TextBox, PushButton
 
-
 #data I gotta send to Kathyrn
-lightFreq = 0
-audioFreq = 0
-runStatusAudio = False
-runStatusLight = False
+lightFreq = 0 #integer: light frequency
+audioFreq = 0 #interger: audio frequency
+runStatusAudio = False #boolean: False implies audio off, True implies audio on
+runStatusLight = False #boolean: False implies light off, True implies light on
 
 def getFreqValue():
-
+    #called upon pressing main button, updateFreqValue
+    #checks user input for both light and audio frequencies
+    #checks for input to be in range
+    #if out of range, resets value to 0 and informs user of error
+    #sends lightFreq and audioFreq to main script
+    
     global lightFreq
     global audioFreq
     global runStatusAudio
@@ -18,34 +22,37 @@ def getFreqValue():
         lightFreq=0
         err1 = True
     else:
-            lightFreq = int(lightFreqInput.value)
-            err1 = False
+        lightFreq = int(lightFreqInput.value)
+        err1 = False
     if ((int(audioFreqInput.value) < 0) or (int(audioFreqInput.value) > 60)):
         audioFreq=0
         err2 = True
     else:
-            audioFreq = int(audioFreqInput.value)
-            err2 = False
+        audioFreq = int(audioFreqInput.value)
+        err2 = False
     if(err2 or err1):
         errFlagBox.value = '*Error*'
     else:
-            errFlagBox.value = 'Frequencies Updated'
+        errFlagBox.value = 'Frequencies Updated'
             
     displayLight.value = "Light Frequency: " + str(lightFreq)
     displayAudio.value = "Audio Frequency: " + str(audioFreq)
     
 def onOffAudio():
-
+    #flips value of boolean runStatusAudio, updates user visuals
+    #send boolean runStatusAudio to main script
+    
     global audioFreq
     global runStatusAudio
     
-   
     runStatusAudio = not runStatusAudio
     
     onOffAudio.text = "Audio status: " + str(runStatusAudio)
     
 def onOffLight():
-
+    #flips value of boolean runStatusLight, updates user visuals
+    #send boolean runStatusLight to main script
+    
     global lightFreq
     global runStatusLight
     
@@ -53,33 +60,24 @@ def onOffLight():
     
     onOffLight.text = "Light status: " + str(runStatusLight)
    
-
-    
-freqGui = App(title="Neurotherapy Control GUI") #start of app
-
-
-
+freqGui = App(title="Neurotherapy Control GUI") #start app
 
 mainDisplay = Text(freqGui, text="Welcome",size=36, font="Roboto") #main display
 
+lightTitle = Text(freqGui,text="Light Frequency",size=24,font="Roboto") #main Light Frequency display (int lightFreq)
+lightFreqInput = TextBox(freqGui, width="40") #accepts user input for (int lightFreq)
 
+audioTitle = Text(freqGui,text="Audio Frequency",size=24,font="Roboto") #main Audio Frequency display (int audioFreq)
+audioFreqInput = TextBox(freqGui, width="40") #accepts user input for (int audioFreq)
 
+updateFreqValue = PushButton(freqGui,command=getFreqValue,text="Update Frequency Values") #updates both (ints audioFreq, lightFreq)
 
-lightTitle = Text(freqGui,text="Light Frequency",size=24,font="Roboto")
-lightFreqInput = TextBox(freqGui, width="40")
-audioTitle = Text(freqGui,text="Audio Frequency",size=24,font="Roboto")
-audioFreqInput = TextBox(freqGui, width="40")
-updateFreqValue = PushButton(freqGui,command=getFreqValue,text="Update Frequency Values")
+displayLight = Text(freqGui,text="No values set",size=24,font="Roboto") #display for current (int lightFreq)
+displayAudio = Text(freqGui,text="No values Set",size=24,font="Roboto") #display for current (int audioFreq)
 
-displayLight = Text(freqGui,text="No values set",size=24,font="Roboto")
-displayAudio = Text(freqGui,text="No values Set",size=24,font="Roboto")
+errFlagBox = Text(freqGui,text="**",size=24,font="Roboto") #displays if there is currently an error in the acceptance of user inputs
 
-errFlagBox = Text(freqGui,text="**",size=24,font="Roboto")
+onOffLight = PushButton(freqGui,command=onOffLight,text="Light status: Standby") #changes status of (boolean runStatusLight
+onOffAudio = PushButton(freqGui,command=onOffAudio,text="Audio status: Standby") #changes status of (boolean runStatusAudio)
 
-onOffLight = PushButton(freqGui,command=onOffLight,text="Light status: Standby")
-onOffAudio = PushButton(freqGui,command=onOffAudio,text="Audio status: Standby")
-
-
-            
-
-freqGui.display()
+freqGui.display() #end app
